12b6eaecf15bb8cf48d9a006782fbd5ae85a283e
niqueladopl <niqueladopl@outlook.com>
2022-11-04 09:23:29 -0300

    atualizacao

--- atividadeAula13/atividadePratica_aula13.py
+++ atividadeAula13/atividadePratica_aula13.py
@@ -20,30 +20,43 @@ while (mainLoop == 1):
         os.system('cls')
 
         print("Cadastro de Itens\n")
-        opCadastro = int(input("1) Cadastrar Novo Item\n2) Voltar\n->"))
-        while (opCadastro != 1) and (opCadastro != 2):
-            opCadastro = int(input(
-                "Opção INVÁLIDA\nDigite 1 para Cadastrar Novo Item ou 2 para Voltar ao Menu\n->"))
+        opCadastro = input("1) Cadastrar Novo Item\n2) Voltar\n->")
+        while (opCadastro != "1") and (opCadastro != "2"):
+            opCadastro = input(
+                "Opção INVÁLIDA\nDigite 1 para Cadastrar Novo Item ou 2 para Voltar ao Menu\n->")
             os.system('cls')
 
-        if (opCadastro == 1):
-            while opCadastro == 1:
+        if (opCadastro == "1"):
+            while (opCadastro == "1"):
                 os.system('cls')
                 print("Cadastrar Novo Item\n")
-                novoItem = {'Título' : input("Título: "), 'duracao' : input("Duração: "), 'sinopse' : input("Sinopse: ")}
+                novoItem = {'nome_item': input("Nome do item: "), 'preco': input(
+                    "Preço: "), 'volume': input("Volume: "), 'quantidade': input("Quantidade disponível: ")}
                 itens.append(novoItem)
 
-                decisao = int(input("\n1) Cadastrar outro item\n2) Voltar para o Menu\n-> "))
-                if (decisao == 2):
-                    opCadastro = 0
-                    op = 0
+                decisao = input("\n1) Cadastrar outro item\n2) Voltar ao Menu Principal\n-> ")
+                while (decisao != "1") and (decisao != "2"):
+                    decisao = input("ERRO! Digite 1 para Cadastrar outro Item ou 2 para Voltar ao Menu Principal\n->")
+                if (decisao == "2"):
+                    opCadastro = "0"
+                    op = "0"
 
-        elif (opCadastro == 2):
+        elif (opCadastro == "2"):
             os.system('cls')
             print("Voltando...")
             op = 0
 
+# Itens Cadastrados
+    while (op == "2"):
+        os.system('cls')
+        if len(itens) == 0:
+            print("Nenhum Item Cadastrado")
+            input("\nAperte ENTER para voltar ao Menu Principal")
+            op = 1
 
+        else:
+            print("A lista não está vazia")
+            input()
 
 # SAIR --- OP=4
     if (op == "4"):
